1369ebe0d362a8d5a657f3cbcce4bd723c858726
Peter Novotnak <yellobes@gmail.com>
2013-03-19 12:31:29 -0500

    updated realm and username location

--- auth.php
+++ auth.php
@@ -4,12 +4,12 @@ include_once('config.php');
 
 // Let's verify our user 
 if (!isset($_SERVER['PHP_AUTH_USER'])) {
-  header('WWW-Authenticate: Basic realm="My Realm"');
+  header('WWW-Authenticate: Basic realm="S67 Admin"');
   header('HTTP/1.0 401 Unauthorized');
   echo 'You need to be authorized to view this page.';
   exit;
 } else {
-  if(($_SERVER['PHP_AUTH_USER'] == 'cmeyer') && ($_SERVER['PHP_AUTH_PW'] == $PASSWORD)){
+  if(($_SERVER['PHP_AUTH_USER'] == $USERNAME) && ($_SERVER['PHP_AUTH_PW'] == $PASSWORD)){
 
   } else {
     echo 'Bad user or pass.';
